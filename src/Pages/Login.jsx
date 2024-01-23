bb0f75f105f0c0c39aad272c383969ab0cbdee98
Yanik Kumar <believemaster@gmail.com>
2024-01-23 23:32:36 +0530

    Login using axios

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,34 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Pages.css";
+import axios from "axios";
+import { Navigate } from "react-router-dom";
 
 function Login() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
+
+  const submit = async (e) => {
+    e.preventDefault();
+
+    await axios.post('login', {
+      email: email,
+      password: password
+    });
+
+    setRedirect(true);
+  }
+  
+  if(redirect) {
+    return <Navigate to='/' />
+  }
+
   return (
     <>
       <main className="form-signin w-100 m-auto">
         <h1 className="h3 mb-3 fw-normal text-center fw-bold">
           Please sign in
         </h1>
-        <div className="form-floating">
-          <input
-            type="email"
-            id="floatingInput"
-            className="form-control rounded-bottom-0"
-            placeholder="name@example.com"
-          />
-          <label htmlFor="floatingInput">Email address</label>
-        </div>
-        <div className="form-floating mb-3">
-          <input
-            type="password"
-            className="form-control rounded-top-0"
-            id="floatingPassword"
-            placeholder="Password"
-          />
-          <label htmlFor="floatingPassword">Password</label>
-        </div>
-        <button className="btn btn-primary w-100 py-2" type="submit">
-          Sign in
-        </button>
+        <form onSubmit={submit}>
+          <div className="form-floating">
+            <input
+              type="email"
+              id="floatingInput"
+              className="form-control rounded-bottom-0"
+              placeholder="name@example.com"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <label htmlFor="floatingInput">Email address</label>
+          </div>
+          <div className="form-floating mb-3">
+            <input
+              type="password"
+              className="form-control rounded-top-0"
+              id="floatingPassword"
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <label htmlFor="floatingPassword">Password</label>
+          </div>
+          <button className="btn btn-primary w-100 py-2" type="submit">
+            Sign in
+          </button>
+        </form>
         <p className="mt-5 mb-3 text-body-secondary text-center">
           &copy; ReactFront {new Date().getFullYear()}
         </p>
