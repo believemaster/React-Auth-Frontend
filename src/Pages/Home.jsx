380e1c7441e370f24e48f404e9f4bd191cd20638
Yanik Kumar <believemaster@gmail.com>
2024-01-23 23:58:15 +0530

    Authorized user home page user data

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,35 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Alert, Col, Row } from "react-bootstrap";
 
 const Home = () => {
+    const [message, setMessage] = useState("");
+    const [variant, setVariant] = useState("warning");
+    useEffect(() => {
+        (
+            async () => {
+                try {
+                    const response = await axios.get('user')
+
+                    console.log(response);
+                    const user = response.data
+
+                    setMessage(`Hello! ${user.first_name} ${user.last_name}`);
+                    setVariant("success")
+                } catch (e) {
+                    setMessage("You are not logged in!")
+                }
+            }
+        )();
+    }, []);
+
     return (
         <>
             <div className="container">
                 <Row className="text-center">
                     <Col md={6} className="mx-auto">
-                        <Alert variant="warning" dismissible>
-                            You are not logged in.
+                        <Alert variant={variant} className="rounded-0" dismissible>
+                            {message}
                         </Alert>
                     </Col>
                 </Row>
--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from "axios";
 
 axios.defaults.baseURL = 'http://localhost:8000/api/';
+axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
